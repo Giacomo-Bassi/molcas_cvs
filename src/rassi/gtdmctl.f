4d36ca7bfef87b36b24cc649b0222ea354b05442
GiacomoBassi <g.bassi@tum.de>
2025-12-05 14:52:47 +0100

    Removed tracking of cmake/custom/qcmaquis.cmake and some print command on src/rassi/gtdmctl.f

--- src/rassi/gtdmctl.f
+++ src/rassi/gtdmctl.f
@@ -995,8 +995,6 @@ C     Defining the Binding energy Ei-Ej
 
 C General 1-particle transition density matrix:
       IF (IF11) THEN
-C Added by Giacomo Bassi for Mpssi debug
-        write(6, *) 'Calling MKTDM1'
         CALL MKTDM1(LSYM1,MPLET1,MSPROJ1,FSBTAB1,
      &              LSYM2,MPLET2,MSPROJ2,FSBTAB2,SSTAB,
      &            OMAP,DET1,DET2,SIJ,NASHT,
@@ -1141,8 +1139,6 @@ C             Write density 1-matrices in AO basis to disk.
                 SIJ=OVERLAP_RASSI(FSBTAB1,FSBTAB2,DET1,DET2)
 #ifdef _DMRG_
               else
-C Added by Giacomo Bassi for Mpssi debug
-                write(6, *) 'Calling from gtdmctl, qcm overlap'
                 sij = qcmaquis_mpssi_overlap(
      &            qcm_prefixes(job1),
      &            ist,
